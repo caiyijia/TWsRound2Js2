d711cc08a9f898704449237ecb0723ebda072e15
Adam Cai <caiyijias@outlook.com>
2019-05-28 22:24:35 +0800

    finished Round2Js2

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const arrays = [
+    ['._.',   '...',   '._.',   '._.',   '...',   '._.',   '._.',   '._.',   '._.',   '._.'],
+    ['|.|',   '..|',   '._|',   '._|',   '|_|',   '|_.',   '|_.',   '..|',   '|_|',   '|_|'],
+    ['|_|',   '..|',   '|_.',   '._|',   '..|',   '._|',   '|_|',   '..|',   '|_|',   '..|']
+]
+
+module.exports = function main(str) {
+    const num = [[],[],[]]
+
+    for(let s of str) {
+        num[0].push(arrays[0][s])
+        num[1].push(arrays[1][s])
+        num[2].push(arrays[2][s])
+    }
+    return num.map(row => row.join(' ') + '\n').join('')
 };
